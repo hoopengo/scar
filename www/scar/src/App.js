580d4d2723536930d5100bac90b9976cd6f161d2
hoopengo <hoopengo@yandex.ru>
2020-04-15 17:42:17 +0300

    'hello'

---
+++ www/scar/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Button } from 'bootstrap-4-react';
+
+function App() {
+  return (
+    <div>
+      <Button primary>help</Button>
+    </div>
+  );
+}
+
+export default App;
